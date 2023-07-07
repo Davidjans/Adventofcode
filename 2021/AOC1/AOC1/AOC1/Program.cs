18e24c2876e9f3b83729166ebeee088d9e7eb289
David <davidjejans@hotmail.nl>
2023-07-07 14:01:25 +0200

    advent of code 2021 that i totally didnt push to github just now

---
+++ 2021/AOC1/AOC1/AOC1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+namespace AOC1
+{
+	class Program
+	{
+       
+        static void Main(string[] args)
+		{
+            String line;
+            int currentNumber = 0;
+            int lastNumber = 0;
+            int larger = 0;
+            int smaller = 0;
+            int theSame = 0;
+            String[] lines = File.ReadLines("D:/HKU/AdventOfCode/AOC1/AOC1/input.txt").ToArray<string>() ;
+            line = "";
+            int[] numberArray =  Array.ConvertAll(lines, int.Parse);
+			for (int i = 0; i < numberArray.Length; i++)
+			{
+                currentNumber += numberArray[i];
+                if (currentNumber > lastNumber)
+                {
+                    line = currentNumber + " (INCREASING)";
+                    larger++;
+                }
+                else if (currentNumber < lastNumber)
+                {
+                    line = currentNumber + " (DECREASING)";
+                    smaller++;
+                }
+                else
+                {
+                    line = currentNumber + " (N/A - no previous measurement)";
+                    theSame++;
+                }
+                Console.WriteLine(line);
+                lastNumber = currentNumber;
+                if (i >= 2)
+                {
+                    currentNumber -= numberArray[i - 2];
+                }
+            }
+            Console.WriteLine("Times increased: " + larger);
+            Console.WriteLine("Times decreased: " + smaller);
+            Console.WriteLine("Times the same: " + theSame);
+            Console.ReadLine();
+        }
+	}
+}
---
+++ 2021/AOC2/AOC2/AOC2/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+class Program
+{
+	
+	static void Main(string[] args)
+	{
+		
+		String[] lines = File.ReadLines("D:/HKU/AdventOfCode/AOC2/input.txt").ToArray<string>();
+
+		Console.WriteLine("Dis is da answer for 1: " + DavidDay2_1(lines));
+
+		Console.WriteLine("Dis is da answer for 2: " + DavidDay2_2(lines));
+
+
+		int DavidDay2_1(string[] lines)
+		{
+			int xPos = 0;
+			int depth = 0;
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].Contains("up"))
+				{
+					depth -= int.Parse(lines[i][lines[i].Length - 1].ToString());
+				}
+				else if (lines[i].Contains("down"))
+				{
+					depth += int.Parse(lines[i][lines[i].Length - 1].ToString());
+				}
+				else if (lines[i].Contains("forward"))
+				{
+					xPos += int.Parse(lines[i][lines[i].Length - 1].ToString());
+				}
+			}
+			return xPos * depth;
+		}
+
+		int DavidDay2_2(string[] lines)
+		{
+			int xPos = 0;
+			int depth = 0;
+			int aim = 0;
+			for (int i = 0; i < lines.Length; i++)
+			{
+				int number = 0;
+				if (lines[i].Contains("up"))
+				{
+					number = int.Parse(lines[i][lines[i].Length - 1].ToString());
+					aim -= number;
+					//depth -= number;
+					
+				}
+				else if (lines[i].Contains("down"))
+				{
+					number = int.Parse(lines[i][lines[i].Length - 1].ToString());
+					aim += number;
+					//depth += number;
+				}
+				else if (lines[i].Contains("forward"))
+				{
+					number = int.Parse(lines[i][lines[i].Length - 1].ToString());
+					xPos += number;
+					depth += number * aim;
+				}
+			}
+			return xPos * depth;
+		}
+	}
+}
